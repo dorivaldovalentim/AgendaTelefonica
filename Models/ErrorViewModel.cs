5c3f3dcca4311fbb546901a37ed3ab09fdde67bf
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-20 13:15:57 +0100

    Adding the basic structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgendaTelefonica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
