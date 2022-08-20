28a899d431fadfd72a5349dfcb97614c5486ad75
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-20 13:22:22 +0100

    Preparei a estrutura básica para o crud de contactos

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaTelefonica.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
