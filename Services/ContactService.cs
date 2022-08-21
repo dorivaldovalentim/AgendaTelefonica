8c5a3748a3d9d988c6d1116cb10b27688dd2d554
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-21 14:43:24 +0100

    Adição de um serviço para a modal de contactos

---
+++ Services/ContactService.cs
@@ -0,0 +1,27 @@
+﻿using AgendaTelefonica.Data;
+using AgendaTelefonica.Models;
+
+namespace AgendaTelefonica.Services
+{
+    public interface IContact
+    {
+        ContactModel Store(ContactModel contact);
+    }
+
+    public class ContactService : IContact
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public ContactService(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ContactModel Store(ContactModel contact)
+        {
+            _dbContext.Contacts.Add(contact);
+            _dbContext.SaveChanges();
+            return contact;
+        }
+    }
+}
