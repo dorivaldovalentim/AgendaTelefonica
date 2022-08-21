1b8b5f4109b4aa5ae6f53d5d903469432e22b4a4
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-21 13:36:39 +0100

    Preparei o meu contexto para a conexão com o banco de dados

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using AgendaTelefonica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTelefonica.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+             
+        }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+    }
+}
