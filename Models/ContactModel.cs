9642b6d5e42950317a2db5ff0f33e894eed9515b
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-21 14:41:58 +0100

    Passando um valor padrão para os campos CreatedAt & UpdatedAt da model ContactModel

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -12,8 +12,8 @@ namespace AgendaTelefonica.Models
         
         public string Phone { get; set; }
         
-        public DateTime? CreatedAt { get; set; }
-        
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
     }
 }
