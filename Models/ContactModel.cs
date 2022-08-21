aafecbd1d8598b14fe23f8c095d6f93f4d36de2c
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-21 13:35:56 +0100

    Preparei a minha model de contactos (database fields as class attributes)

---
+++ Models/ContactModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AgendaTelefonica.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Phone { get; set; }
+        
+        public DateTime? CreatedAt { get; set; }
+        
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
