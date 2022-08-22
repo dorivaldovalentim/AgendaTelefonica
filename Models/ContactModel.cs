d169282ac894dbf3be08cc6e528524e782b0c84f
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2022-08-22 20:39:46 +0100

    Tornei nullable os atributos Name, Phone e Email da model de contactos

--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -6,11 +6,11 @@ namespace AgendaTelefonica.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
         
-        public string Email { get; set; }
+        public string? Email { get; set; }
         
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
         
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
 
